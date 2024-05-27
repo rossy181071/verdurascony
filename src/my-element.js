67504c4105ab655c976d7c1f7f683ca8a093d8e1
Rossy Lopez <rossy181071@hotmail.com>
2024-05-27 14:30:05 -0600

    Created verduras cony page

--- src/my-element.js
+++ src/my-element.js
@@ -1,7 +1,7 @@
 import { LitElement, css, html } from 'lit'
-import litLogo from './assets/lit.svg'
-import viteLogo from '/vite.svg'
-
+import './frutas-cony.js'
+import './semillas-cereales.js'
+import './verduras-cony.js'
 /**
  * An example element.
  *
@@ -12,46 +12,50 @@ export class MyElement extends LitElement {
   static get properties() {
     return {
       /**
-       * Copy for the read the docs hint.
+       * The number of page currently displayed
        */
-      docsHint: { type: String },
-
-      /**
-       * The number of times the button has been clicked.
-       */
-      count: { type: Number },
+      position: { type: Number },
     }
   }
 
   constructor() {
     super()
-    this.docsHint = 'Click on the Vite and Lit logos to learn more'
-    this.count = 0
+    this.position = 0
+
+  }
+
+  setPrevious(e) {
+    if (this.position === 0) {
+      this.position = 2
+    } else {
+      this.position--
+    }
+  }
+
+  setNext(e) {
+    if (this.position === 2) {
+      this.position = 0
+    } else {
+      this.position++
+    }
   }
 
   render() {
     return html`
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src=${viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://lit.dev" target="_blank">
-          <img src=${litLogo} class="logo lit" alt="Lit logo" />
-        </a>
-      </div>
-      <slot></slot>
-      <div class="card">
-        <button @click=${this._onClick} part="button">
-          count is ${this.count}
-        </button>
-      </div>
-      <p class="read-the-docs">${this.docsHint}</p>
+      <button @click="${this.setPrevious}">Anterior</button>
+      <button @click="${this.setNext}">Siguiente</button>
+      <h1>${this.position}</h1>
+      ${this.position === 0 ? html` <verduras-cony></verduras-cony>` : ''}
+      ${this.position === 1 ? html`<frutas-cony></frutas-cony>` : ''}
+      ${this.position === 2 ? html`<semillas-cereales></semillas-cereales>` : ''}
+    
+    </div>
+    
     `
   }
 
-  _onClick() {
-    this.count++
-  }
+ 
 
   static get styles() {
     return css`
@@ -61,69 +65,8 @@ export class MyElement extends LitElement {
         padding: 2rem;
         text-align: center;
       }
-
-      .logo {
-        height: 6em;
-        padding: 1.5em;
-        will-change: filter;
-        transition: filter 300ms;
-      }
-      .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-      }
-      .logo.lit:hover {
-        filter: drop-shadow(0 0 2em #325cffaa);
-      }
-
-      .card {
-        padding: 2em;
-      }
-
-      .read-the-docs {
-        color: #888;
-      }
-
-      a {
-        font-weight: 500;
-        color: #646cff;
-        text-decoration: inherit;
-      }
-      a:hover {
-        color: #535bf2;
-      }
-
-      ::slotted(h1) {
-        font-size: 3.2em;
-        line-height: 1.1;
-      }
-
-      button {
-        border-radius: 8px;
-        border: 1px solid transparent;
-        padding: 0.6em 1.2em;
-        font-size: 1em;
-        font-weight: 500;
-        font-family: inherit;
-        background-color: #1a1a1a;
-        cursor: pointer;
-        transition: border-color 0.25s;
-      }
-      button:hover {
-        border-color: #646cff;
-      }
-      button:focus,
-      button:focus-visible {
-        outline: 4px auto -webkit-focus-ring-color;
-      }
-
-      @media (prefers-color-scheme: light) {
-        a:hover {
-          color: #747bff;
-        }
-        button {
-          background-color: #f9f9f9;
-        }
-      }
+    
+      
     `
   }
 }
